8c06c4b9608836a66c77d119f56511c6c8740ff4
Daniel Martinez <daniel.martinez@mexicoder.com.mx>
2015-08-26 17:51:54 -0500

    Add subscribe file, index template and implemente logic for subscribe with a service that provide badges.

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
+var subsocket = require('../lib/subscribe');
+
+server.listen(3000, function(){
+	console.log('Server running at port %d', 3000);
+});
+
 /*
 * Serve static assets out of public
 */
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-	console.log('Server running at port %d', 3000);
-});
-
 app.get('/', function(req, res){
 	res.sendfile('./public/index.html');
+});
+
+io.socket.on('connection', function(socket){
+	model.get(function(err, data){
+		if (err) return;
+		data.forEach(function(badge){
+			socket.emit('badge', badge);
+		});
+	});
+});
+
+subsocket.on('message', function(message){
+	io.socket.emit('badge', message);
 });

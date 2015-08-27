eee02d11dd66be11fe0b540d00bdcc4b5f75d4c0
Daniel Martinez <daniel.martinez@mexicoder.com.mx>
2015-08-27 10:53:39 -0500

    Add css, and js files, as well as finished subscribe funcionality

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var badges = require('./models/badges');
 
-var subsocket = require('../lib/subscribe');
+var subsocket = require('./lib/subscribe');
 
 server.listen(3000, function(){
 	console.log('Server running at port %d', 3000);
@@ -20,8 +21,8 @@ app.get('/', function(req, res){
 	res.sendfile('./public/index.html');
 });
 
-io.socket.on('connection', function(socket){
-	model.get(function(err, data){
+io.sockets.on('connection', function(socket){
+	badges.get(function(err, data){
 		if (err) return;
 		data.forEach(function(badge){
 			socket.emit('badge', badge);
@@ -30,5 +31,5 @@ io.socket.on('connection', function(socket){
 });
 
 subsocket.on('message', function(message){
-	io.socket.emit('badge', message);
+	io.sockets.emit('badge', message);
 });
---
+++ public/js/nodetutorial.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(function(){
+	var socket = io.connect();
+
+	socket.on('badge', function(badge){
+		var $img = $('<img src="'+badge.badge_id+'" alt="Code School badge" >');
+		$('body').prepend($img);
+	});
+});
